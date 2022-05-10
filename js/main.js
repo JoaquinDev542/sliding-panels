3b70037fe98730bc015eff18efa502534711ca91
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-05-10 23:22:25 +0200

    upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+const $toggleButton = document.querySelector(".toggleButton");
+const $list = document.querySelector(".list");
+
+const $projects = document.querySelector(".projects");
+
+const $projectFirst = document.querySelector(".project--first");
+const $projectSecond = document.querySelector(".project--second");
+const $projectThird = document.querySelector(".project--third");
+const $projectFourth = document.querySelector(".project--fourth");
+
+const $information = document.querySelector(".information");
+
+    const toggleButtonFunction = function () {
+        $toggleButton.addEventListener("click" , () => {
+            $projects.classList.toggle("active");
+            $list.classList.toggle("active");
+        })
+    }
+
+    toggleButtonFunction();
+
+
+// Transition transition en las imagenes background-color
