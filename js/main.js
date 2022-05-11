0da837afc4fd9b828b51b38f610fb9922cf573f9
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-05-11 13:42:33 +0200

    upload

--- js/main.js
+++ js/main.js
@@ -1,36 +1,55 @@
+// Obtener Elementos HTML
+
+// ToggleButton y List 
 const $toggleButton = document.querySelector(".toggleButton");
 const $list = document.querySelector(".list");
 
+// ToggleButtonImg
 const $toggleButtonImg = document.querySelector(".toggleButton__img");
 
+// Projects
 const $projects = document.querySelector(".projects");
 
+// ProjectContent
+const $projectContent = document.querySelector(".project__content");
+
+// ProjectFirst / ProjectSecond / ProjectThird / ProjectFourth
 const $projectFirst = document.querySelector(".project--first");
 const $projectSecond = document.querySelector(".project--second");
 const $projectThird = document.querySelector(".project--third");
 const $projectFourth = document.querySelector(".project--fourth");
 
+// Information
 const $information = document.querySelector(".information");
 
+// FUNCTION ToggleButton
     const toggleButtonFunction = function () {
         $toggleButton.addEventListener("click" , () => {
             $projects.classList.toggle("active");
             $list.classList.toggle("active");
 
-            toggleButtonImgChangeFunction();
+            // toggleButtonImgChangeFunction();
+            // toggleButtonImgBackFunction();
+
         })
     }
     toggleButtonFunction();
 
-    const toggleButtonImgChangeFunction = function () {
-        // $toggleButtonImg
-        $toggleButtonImg.src = "assets/image/icons/close_black_24dp.svg";
-    }
+    
+
+// FUNCTION  
+    // const toggleButtonImgChangeFunction = function () {
+    
+    //     $toggleButtonImg.src = "assets/image/icons/close_black_24dp.svg";
+    // }
+
+    // $toggleButtonImg.addEventListener("click" , () => {
+    //     $toggleButtonImg.src = "assets/image/icons/menu_black_24dp.svg";
+    // })
 
     // const toggleButtonImgBackFunction = function () {
     //     $toggleButtonImg.src = "assets/image/icons/menu_black_24dp.svg";
     // }
-    // toggleButtonImgBackFunction();
 
     // const goToProjectsFunction = function () {
     //     $projectFirst.addEventListener("click" , () => {
@@ -40,10 +59,12 @@ const $information = document.querySelector(".information");
     //         $projectThird.classList.add("active");
     //         $projectFourth.classList.add("active");
 
+    //         $projectContent.classList.add("active");
+
     //     })
     // }
     // goToProjectsFunction();
 
 
-//  eliminar imagen dentro de information , ::root
+//  eliminar imagen dentro de information , acomodar código html,js 
 
